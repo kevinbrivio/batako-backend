673a80fb9f239804f8e5e7baaba5b39dae157287
itzKv <kenkenku6@gmail.com>
2025-10-14 15:05:28 +0700

    fix: syntax errors

--- internal/store/production.go
+++ internal/store/production.go
@@ -114,10 +114,11 @@ func (s *ProductionStore) GetByID(ctx context.Context, pID string) (*models.Prod
 func (s *ProductionStore) Update(ctx context.Context, p *models.Production) error {
 	query := `
 		UPDATE productions
-		SET quantity = $2, cement_used = $3, sand_used = $4,
+		SET quantity = $2, cement_used = $3, sand_used = $4
 		WHERE id = $1
-		RETURNING created_at
+		RETURNING updated_at
 	`
+
 
 	ctx, cancel := context.WithTimeout(ctx, time.Second * 5)
 	defer cancel()
