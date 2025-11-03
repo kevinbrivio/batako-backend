19bc2a8dce52f1c5df5cbed4d2a87dd3ef578080
itzKv <kenkenku6@gmail.com>
2025-11-03 11:54:08 +0700

    add: cement_stock table

---
+++ cmd/migrate/migrations/000009_create_cement_stock_table.up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS cement_types (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cement_stocks(
+    id VARCHAR(36) PRIMARY KEY,
+    cement_type_id INT NOT NULL REFERENCES cement_types(id),
+    quantity DOUBLE PRECISION NOT NULL CHECK (quantity >= 0),
+    price_per_bag DOUBLE PRECISION NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS production_cement_usage (
+    id SERIAL PRIMARY KEY,
+    production_id VARCHAR(36) NOT NULL REFERENCES productions(id) ON DELETE CASCADE,
+    cement_type_id INT NOT NULL REFERENCES cement_types(id),
+    cement_used DOUBLE PRECISION NOT NULL CHECK (cement_used >= 0)
+);
