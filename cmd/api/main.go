7f3789878fa2d21ffbb11cf43463d1853cb1b0e8
itzKv <kenkenku6@gmail.com>
2025-11-19 10:08:55 +0700

    add: crud cement

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -17,37 +17,38 @@ import (
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-        log.Println("No .env file found, using system environment variables")
-    }
+		log.Println("No .env file found, using system environment variables")
+	}
 
 	// OPEN DB
 	var connStr = os.Getenv("DATABASE_URL")
-	log.Printf("Connecting to: %s (local dev or prod)", connStr[:len("postgres://") + 10]) // Truncated log
+	log.Printf("Connecting to: %s (local dev or prod)", connStr[:len("postgres://")+10]) // Truncated log
 
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        log.Fatal("Failed to open DB: ", err.Error())
-    }
-    defer db.Close()
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Failed to open DB: ", err.Error())
+	}
+	defer db.Close()
 
 	// Ping to verify live connection
-    err = db.Ping()
-    if err != nil {
-        log.Fatal("DB connection failed (ping): ", err.Error())
-    }
-    log.Println("Successfully connected and pinged DB")
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("DB connection failed (ping): ", err.Error())
+	}
+	log.Println("Successfully connected and pinged DB")
 
-    _, err = db.Exec("SET search_path TO my_schema")
-    if err != nil {
-        log.Fatal("Schema set failed: ", err.Error())
-    }
-    log.Println("Schema set to my_schema")
+	_, err = db.Exec("SET search_path TO my_schema")
+	if err != nil {
+		log.Fatal("Schema set failed: ", err.Error())
+	}
+	log.Println("Schema set to my_schema")
 
 	storage := store.NewStorage(db)
 	prodHandler := handlers.NewProductionHandler(storage)
 	transactionHandler := handlers.NewTransactionHandler(storage)
 	salaryHandler := handlers.NewSalaryStorage(storage)
-	
+	cementStockHandler := handlers.NewCementStockHandler(storage)
+	sandPurchaseHandler := handlers.NewSandPurchaseHandler(storage)
 	// Run scheduler -> Calculate weekly salary
 	salaryStorage := storage.Salary
 	go salaryStorage.StartSchedulers(context.Background())
@@ -57,10 +58,10 @@ func main() {
 	r.Use(middleware.Logger)
 
 	// Health check endpoint
-    r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-        w.WriteHeader(http.StatusOK)
-        w.Write([]byte("OK"))
-    })
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	r.Route("/productions", func(r chi.Router) {
 		r.Post("/", prodHandler.CreateProduction)
@@ -71,7 +72,7 @@ func main() {
 		r.Put("/{id}", prodHandler.UpdateProduction)
 		r.Delete("/{id}", prodHandler.DeleteProduction)
 	})
-	
+
 	r.Route("/transactions", func(r chi.Router) {
 		r.Post("/", transactionHandler.CreateTransaction)
 		r.Get("/", transactionHandler.GetAllTransactions)
@@ -87,7 +88,23 @@ func main() {
 		r.Get("/weekly", salaryHandler.GetWeeklySalary)
 		r.Get("/monthly", salaryHandler.GetMonthlySalaries)
 	})
-	
+
+	r.Route("/stock", func(r chi.Router) {
+		r.Route("/cement", func(r chi.Router) {
+			r.Post("/", cementStockHandler.AddCementStock)
+			r.Put("/{id}", cementStockHandler.UpdateCementStock)
+			r.Get("/monthly", cementStockHandler.GetMonthlyCementStock)
+			r.Get("/{type}", cementStockHandler.GetCementStocksByType)
+			r.Delete("/{id}", cementStockHandler.DeleteCementStock)
+		})
+		r.Route("/sand", func(r chi.Router) {
+			r.Post("/", sandPurchaseHandler.AddSandPurchase)
+			r.Put("/{id}", sandPurchaseHandler.UpdateSand)
+			// r.Get("/monthly", sandPurchaseHandler.GetMonthlySandPurchase)
+			// r.Delete("{id}", sandPurchaseHandler.DeleteCementStock)
+		})
+	})
+
 	log.Println("Server running at :8080")
-    log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
